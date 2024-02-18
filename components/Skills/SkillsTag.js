2492630d03ca318bf211566fc39d20c2bcb4d643
dhairya2001 <dhairyachhabra3072@gmail.com>
2024-02-18 22:20:59 +0530

    imageChanges-II

--- components/Skills/SkillsTag.js
+++ components/Skills/SkillsTag.js
@@ -1,25 +1,26 @@
 import { useMediaQuery } from "@chakra-ui/react";
-import Image from "next/image";
+// import Image from "next/image";
+import { Image } from "@chakra-ui/react";
 
 const links=[
-    '/git.png',
-    '/assets/skills/css.png',
-    '/assets/skills/html.png',
-    '/assets/skills/javascript.png',
-    '/assets/skills/mongodb.png',
-    '/assets/skills/nodejs.png',
-    '/assets/skills/postgre.png',
-    '/assets/skills/python.png',
-    '/assets/skills/redux.png',
-    '/assets/skills/typescript.png',
-    '/assets/skills/tailwind.png',
+    'git.png',
+    'assets/skills/css.png',
+    'assets/skills/html.png',
+    'assets/skills/javascript.png',
+    'assets/skills/mongodb.png',
+    'assets/skills/nodejs.png',
+    'assets/skills/postgre.png',
+    'assets/skills/python.png',
+    'assets/skills/redux.png',
+    'assets/skills/typescript.png',
+    'assets/skills/tailwind.png',
 ]
 
 const SkillsTag=()=>{
     const skills=links.map((link,index)=>{
         return(
             <div key={index} style={{display:"flex",justifyContent:"center",alignItems:"center",marginLeft:"8px",marginRight:"8px",height:"100px",width:"100px",borderRadius:"50%",backgroundColor:"#7FAEC5"}}>
-                <Image src={link} height={80} width={80}/>
+                <Image src={link} style={{height:"80px",width:"80px"}}/>
             </div>
         )
     })
