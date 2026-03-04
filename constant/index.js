cc714fe6be3465b49e982dd9460c2f5634c357ac
dhairya2001 <dhairyachhabra3072@gmail.com>
2026-03-04 13:18:07 -0800

    updated portfolio

--- constant/index.js
+++ constant/index.js
@@ -1,37 +1,44 @@
 export const work = [
   {
-    companyName: "Wordcorn",
-    desingation: "Software Engineer Intern",
-    companyLogo: "wordcorn.jpeg",
+    companyName: "SDSU",
+    desingation: "TA Software Engineer",
+    companyLogo: "sdsu_rf.jpeg",
     desc: [
-      "Built a responsive, mobile-first UI with NextJS and TypeScript, enhancing user retention and accessibility while supporting personalized GenAI-driven content delivery across devices.",
-      "Scaled the web app from 100 to 1,500+ users, achieving 15x user growth through performance optimizations and GenAI-powered content delivery",
-      "Integrated GenAI APIs to auto-generate personalized quizzes and vocabulary drills, enhancing learning efficiency by 40%. Refactored application architecture to support modular feature rollout and improve deployment cycles.",
-      "Reduced backend response time by 35% by optimizing data fetch patterns and caching logic with Supabase."
+      // "Containerized the MRIQC pipeline using Docker to create a consistent and reproducible environment for processing MRI data, which reduced setup time by 30% and ensured reliable quality control across different systems.",
+      // "Analyzed MRI data and performed ETL to clean and process large datasets; generated detailed visual reports to support research and clinical diagnosis, improving understanding and assessment of ASD-related neuroanatomical features.",
+      "Developed automated grading workflows using Java and JUnit, increasing grading efficiency by 40%",
+      "Led a team of 6 to train and deploy a multi agent Generative AI-based Chatbot assistant on AWS SageMaker employing Python, Flask and FastAPI, improving University’s internal portal information retrieval accuracy by 20%.",
+      "Mentored 200+ students on debugging, refactoring, and software engineering improving overall learning outcomes."
     ],
-    duration: "May 2025 - Aug. 2025",
+    duration: "Sept 2024 - Present",
     location: "San Diego,USA",
   },
   {
-    companyName: "SDSU Research Foundation",
-    desingation: "Graduate Student Assistant",
-    companyLogo: "sdsu_rf.jpeg",
+    companyName: "Wordcorn",
+    desingation: "Software Engineer",
+    companyLogo: "wordcorn.jpeg",
     desc: [
-      "Containerized the MRIQC pipeline using Docker to create a consistent and reproducible environment for processing MRI data, which reduced setup time by 30% and ensured reliable quality control across different systems.",
-      "Analyzed MRI data and performed ETL to clean and process large datasets; generated detailed visual reports to support research and clinical diagnosis, improving understanding and assessment of ASD-related neuroanatomical features."
+      "Engineered end-to-end full-stack functionality using ReactJS, TypeScript on the frontend and backend APIs, supporting authentication, user progress tracking, and personalized learning workflows for an educational web platform.",
+      "Optimized application performance by configuring Cloudflare edge caching for frequently accessed images, reducing re- peated origin fetches and lowering asset load latency by ∼25%.",
+      "Implemented GenAI APIs for content summarization and recommendation logic, reducing content curation effort."
     ],
-    duration: "Sept 2024 - Dec 2024",
-    location: "San Diego,USA",
+    duration: "May 2025 - Aug. 2025",
+    location: "San Jose,USA",
   },
   {
     companyName: "AFour Technologies",
     desingation: "Software Engineer",
     companyLogo: "afour_technologies_logo.png",
     desc: [
-      "Automated deployement and scaling of AI-powered React apps on AWS using CI/CD pipelines and Terraform, improving uptime to 99.9% and a 20% boost in deployment efficiency.",
-      "Built a resignation portal for the ERP system using ReactJS, NodeJS, and MongoDB, leveraging microservices and an event-driven architecture to improve system workflows and user experience.",
-      "Refactored complex legacy code and optimized critical business logic, cutting front-end compile time by 10% to significantly boost developer efficiency and accelerate product release cycles.",
-      "Optimized Java APIs to retrieve only relevant data, improving data handling efficiency and reducing response time, which enhanced overall software performance by 50%"
+      "Re-architected legacy PHP-based systems, migrated core functionality to React and NodeJS, improving responsiveness and backend maintainability while reducing development time by 20%.",
+      "Built and Implemented a resignation portal using ReactJS, and MongoDB, implementing Docker containerization and automated testing with Jest, reducing manual HR effort by 30%.",
+      "To support real-time ad performance tracking, built an analytics dashboard using React and Redux with an event-driven data flow, integrating APIs through Python middleware to handle live updates and ensure consistent state management.",
+      "Coordinated closely with cross-functional teams like senior engineers, QA, and stakeholders in an Agile environment to deliver features incrementally and address production issues.",
+      "Reduced MTTR by 40% by implementing centralized monitoring and observability using AWS CloudWatch and Grafana dashboards to track API latency, error rates, and system health across production services."
+      // "Automated deployement and scaling of AI-powered React apps on AWS using CI/CD pipelines and Terraform, improving uptime to 99.9% and a 20% boost in deployment efficiency.",
+      // "Built a resignation portal for the ERP system using ReactJS, NodeJS, and MongoDB, leveraging microservices and an event-driven architecture to improve system workflows and user experience.",
+      // "Refactored complex legacy code and optimized critical business logic, cutting front-end compile time by 10% to significantly boost developer efficiency and accelerate product release cycles.",
+      // "Optimized Java APIs to retrieve only relevant data, improving data handling efficiency and reducing response time, which enhanced overall software performance by 50%"
     ],
     duration: "Mar 2023 - Mar 2024",
     location: "Pune,India",
@@ -41,9 +48,13 @@ export const work = [
     desingation: "FullStack Developer Intern",
     companyLogo: "digilytics.png",
     desc: [
-      "Led a team of 5 developers to build a No-Code web application with drag-and-drop functionality, enabling 500+ non-technical users to create static websites easily, increasing their sales by around 20%",
-      "Engineered scalable and responsive components using JavaScript frameworks within an Agile, Scrum workflow; built distributed systems to improve performance and support enterprise-grade development.",
-      "Enhanced code quality by following clean code practices and designing a user-friendly UI, which improved user experience by 30% and maintainability by reducing code complexity by 25%, while increasing code readability and enabling faster future enhancements."
+      "To reduce technical barriers for small businesses, Engineered a no-code web platform using React and REST APIs, allowing 500+ businesses to launch webpages and increase sales by∼20%.",
+      "To improve data access speed and scalability, Optimized data access by implementing Redis caching and optimized data storage using PostgreSQL, reducing repeated database queries and improving responsiveness under load.",
+      "Streamlined frontend components using test-driven development(TDD) and modular design patterns which reduced code complexity by∼25%, collaborated with the UX team to improve user experience by∼30% ."
+
+      // "Led a team of 5 developers to build a No-Code web application with drag-and-drop functionality, enabling 500+ non-technical users to create static websites easily, increasing their sales by around 20%",
+      // "Engineered scalable and responsive components using JavaScript frameworks within an Agile, Scrum workflow; built distributed systems to improve performance and support enterprise-grade development.",
+      // "Enhanced code quality by following clean code practices and designing a user-friendly UI, which improved user experience by 30% and maintainability by reducing code complexity by 25%, while increasing code readability and enabling faster future enhancements."
     ],
     duration: "Dec 2021 - May 2022",
     location: "Pune,India",
